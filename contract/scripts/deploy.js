fb1afa7d6d76a2b8f0ae935cda5f14275fd1c882
Hascoin <70804507+0xhascoin@users.noreply.github.com>
2023-03-09 20:33:29 +0000

    finished mainnet contract and tested it

--- contract/scripts/deploy.js
+++ contract/scripts/deploy.js
@@ -1,17 +1,39 @@
 const main = async () => {
-  const nftContractFactory = await hre.ethers.getContractFactory('LensGardenNFT');
+  const nftContractFactory = await hre.ethers.getContractFactory('LGNFT');
   const nftContract = await nftContractFactory.deploy();
   await nftContract.deployed();
   console.log("Contract deployed to:", nftContract.address);
 
-  let txn = await nftContract.mintNFTOne("https://lens-garden-api.hasanelmi.repl.co/api/nft/1")
+  // let txn = await nftContract.mintDynamicNFT("https://lens-garden-api.hasanelmi.repl.co/api/address/0x913261b57E93F4Ca245f09006D56AA764D1a8C2e")
+  // // Wait for it to be mined.
+  // await txn.wait()
+  // console.log("Minted Dynamic NFT.");
+
+  // txn = await nftContract.mintDynamicNFT("https://lens-garden-api.hasanelmi.repl.co/api/address/0x913261b57E93F4Ca245f09006D56AA764D1a8C2e")
+  // // Wait for it to be mined.
+  // await txn.wait()
+  // console.log("Minted Dynamic NFT.");
+
+  txn = await nftContract.mintLevelNFT("https://lens-garden-api.hasanelmi.repl.co/api/nft/7", 7)
   // Wait for it to be mined.
   await txn.wait()
-  console.log("Minted NFT #1")
-  txn = await nftContract.mintNFTNine("https://lens-garden-api.hasanelmi.repl.co/api/nft/9")
+  console.log("Minted NFT #7.");
+
+  txn = await nftContract.mintLevelNFT("https://lens-garden-api.hasanelmi.repl.co/api/nft/7", 7)
   // Wait for it to be mined.
   await txn.wait()
-  console.log("Minted NFT #2")
+  console.log("Minted NFT #7.");
+
+  txn = await nftContract.mintLevelNFT("https://lens-garden-api.hasanelmi.repl.co/api/nft/6", 6)
+  // Wait for it to be mined.
+  await txn.wait()
+  console.log("Minted NFT # 6.");
+
+  txn = await nftContract.mintLevelNFT("https://lens-garden-api.hasanelmi.repl.co/api/nft/6", 6)
+  // Wait for it to be mined.
+  await txn.wait()
+  console.log("Minted NFT # 6.");
+
 }
 
 const runMain = async () => {
